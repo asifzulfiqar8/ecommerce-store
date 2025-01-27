9f0273c400c8d8b74e828d3adf6f007495acac0d
farhanrasheed1121 <farhanrasheed1121@gmail.com>
2025-01-27 19:14:52 +0500

    user added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,25 +1,30 @@
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    const {name, email, password} = req.body;
-    
-    try {
-        const userExists = await User.findOne({email})
-        if (userExists) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        
-        const user = await User.create({name, email, password})
-        res.status(201).json({user, message: "User created successfully"});
-    } catch (error) {
-        res.status(500).json({message: error.message})
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+
+    const user = await User.create({ name, email, password });
+    res
+      .status(201)
+      .json({
+        user: { name: user.name, email: user.email, role: user.role },
+        message: "User created successfully",
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const login = async (req, res) => {
-    res.send('login route called');
-}
+  res.send("login route called");
+};
 
 export const logout = async (req, res) => {
-    res.send('logout route called');
-}
+  res.send("logout route called");
+};
